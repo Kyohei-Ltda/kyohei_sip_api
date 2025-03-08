272058ccd9c157c816a45d6d2d8cbb38725edc35
Simon Capriles <simon.capriles@kyohei.bo>
2025-03-08 11:32:16 -0400

    Starting for version 18

--- __manifest__.py
+++ __manifest__.py
@@ -1,34 +1,29 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "kyohei_bank_integrations",
+    'name': "Integraciones bancarias",
 
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
-    'description': """
-Long description of module's purpose
+    'summary': """
+    Integre su Odoo con las API de los bancos de Bolivia
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'description': """
+Aproveche las integraciones de los bancos en Bolivia
+================================================================================
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
+Después de instalar el módulo obtendrá:
+    * Generación de códigos QR para cobros por sus servicios
+    * Extractos bancarios automáticamente conciliados
+    """,
+    'author': "Kyohei Ltda.",
+    'website': "https://localizacion.kyohei.bo/",
+    'category': 'Accounting/Localizations',
+    'countries': ['bo'],
+    'version': '18.0.0.1',
+    'depends': ['account'],
+    'license': 'Other proprietary',
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    ]
 }
-
---
+++ models/account_journal.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class KyoheiBankIntegrationAccountJournal(models.Model):
+    _inherit = 'account.journal'
+
+
