dcea146cea78cbc86d02565959fa7a23b9d7cbf0
Simon Capriles <simon.capriles@kyohei.bo>
2025-03-12 16:05:33 -0400

    Correcting server response

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -4,6 +4,7 @@
 from odoo import http
 from odoo.http import Response, request
 from datetime import datetime
+import json
 
 
 class KyoheiSipApiControllers(http.Controller):
@@ -11,13 +12,13 @@ class KyoheiSipApiControllers(http.Controller):
     def confirm_sip_qr_payment(self):
         data = request.httprequest.json
         payment_ref = data.get('alias')
-        sip_qr_id = request.env['sip.qr'].sudo().search([('ref', '=', payment_ref), ('state', '=', 'pendiente')], limit=1)
+        sip_qr_id = request.env['sip.qr'].sudo().search([('ref', '=', payment_ref)], limit=1)
         sip_qr_id.sudo().write({'state': 'pagado'})
         process_date_str = data.get('fechaproceso')
         if sip_qr_id:
             bank_statement_line_id = request.env['account.bank.statement.line'].sudo().search([('payment_ref', '=', payment_ref)])
             if not bank_statement_line_id:
-                statement_line_id = request.env['account.bank.statement.line'].sudo().create({
+                request.env['account.bank.statement.line'].sudo().create({
                     'date': datetime.strptime(process_date_str, "%b %d, %Y %I:%M:%S %p"),
                     'journal_id': sip_qr_id.journal_id.id,
                     'currency_id': sip_qr_id.currency_id.id,
@@ -28,13 +29,14 @@ class KyoheiSipApiControllers(http.Controller):
                     'transaction_type': 'QR SIP'
                 })
                 return Response(
-                    '{"codigo": "0000", "mensaje":"Registro exitoso"}',
+                    json.dumps({"codigo": "0000", "mensaje": "Registro exitoso"}),
                     content_type='application/json;charset=utf-8',
                     status=200
                 )
         else:
             return Response(
-                '{"codigo": "9999", "mensaje":"No existe el QR en la base de datos"}',
+                json.dumps({"codigo": "9999", "mensaje": "No existe el QR en la base de datos"}),
                 content_type='application/json;charset=utf-8',
                 status=500
             )
+        Response()
