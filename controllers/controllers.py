9f06d6ac7f7160bc384fac084338563418e46895
Simon Capriles <simon.capriles@kyohei.bo>
2025-03-12 17:21:44 -0400

    Correcting code for api response

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -5,21 +5,26 @@ from odoo import http
 from odoo.http import Response, request
 from datetime import datetime
 import json
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class KyoheiSipApiControllers(http.Controller):
-    @http.route('/sip/confirmaPago', type='json', csrf=False, auth="public", methods=['POST'])
+    @http.route('/sip/confirmaPago', type='http', csrf=False, auth="public", methods=['POST'])
     def confirm_sip_qr_payment(self):
         data = request.httprequest.json
         payment_ref = data.get('alias')
         sip_qr_id = request.env['sip.qr'].sudo().search([('ref', '=', payment_ref)], limit=1)
         sip_qr_id.sudo().write({'state': 'pagado'})
-        process_date_str = data.get('fechaproceso')
+        timestamp_ms = data.get('fechaproceso', 0)
+        process_date = datetime.fromtimestamp(timestamp_ms / 1000.0) if timestamp_ms else datetime.now()
+        _logger.exception("SIP notification: %s", str(data))
         if sip_qr_id:
             bank_statement_line_id = request.env['account.bank.statement.line'].sudo().search([('payment_ref', '=', payment_ref)])
             if not bank_statement_line_id:
                 request.env['account.bank.statement.line'].sudo().create({
-                    'date': datetime.strptime(process_date_str, "%b %d, %Y %I:%M:%S %p"),
+                    'date': process_date,
                     'journal_id': sip_qr_id.journal_id.id,
                     'currency_id': sip_qr_id.currency_id.id,
                     'payment_ref': sip_qr_id.ref,
@@ -29,7 +34,7 @@ class KyoheiSipApiControllers(http.Controller):
                     'transaction_type': 'QR SIP'
                 })
             return Response(
-                '{"codigo": "0000", "mensaje": "Registro exitoso"}',
+                json.dumps({"codigo": "0000", "mensaje": "Registro exitoso"}),
                 content_type='application/json;charset=utf-8',
                 status=200
             )
